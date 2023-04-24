58514f89a41978e0181d74437c6b153dd841dd86
Vyacheslav <slava.shilin07@mail.ru>
2023-04-24 13:16:14 +0300

    Added ResetAllTerrains terminal command

---
+++ TerminalCommands.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace GroundReset;
+
+public static class TerminalCommands
+{
+    private static string modName => Plugin.ModName;
+
+    [HarmonyPatch(typeof(Terminal), nameof(Terminal.InitTerminal))]
+    internal class AddChatCommands
+    {
+        private static void Postfix()
+        {
+            _ = new Terminal.ConsoleCommand(modName, $"Manages the {modName.Replace(".", "")} commands.",
+                args =>
+                {
+                    if (!Plugin.configSync.IsAdmin && !ZNet.instance.IsServer())
+                    {
+                        args.Context.AddString("You are not an admin on this server.");
+                        return;
+                    }
+
+                    if (args.Length == 2 && args[1] == "ResetAllTerrains")
+                    {
+                        Reseter.ResetAllTerrains();
+                    }
+
+                    args.Context.AddString("ResetAllTerrains");
+                },
+                optionsFetcher: () => new List<string> { "ResetAllTerrains" });
+        }
+    }
+}
